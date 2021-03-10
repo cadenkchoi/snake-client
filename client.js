e9f11d3e4d80f90759240ffc9292ab5c0c52127c
Caden Choi <cadenc.7@gmail.com>
2021-03-10 21:37:24 +0000

    added ctrl-c exit function and updated to help establish UI

--- client.js
+++ client.js
@@ -5,12 +5,17 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
-  // interpret incoming data as text
+
+  conn.on('connect', (connect) => {
+    console.log("Connection Established Successfully");
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: CKC');
+  })
+
   conn.setEncoding('utf8'); 
-  // conn.on('connect', () => {
-  //   conn.write(data);
-  // })
-  // allows us to receive messages back from server
+
   conn.on('data', (data) => { 
     console.log('New message from the server:', data)
   })
